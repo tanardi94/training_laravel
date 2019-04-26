ec4b7c19d176717f95ea23003a3f963a1774db28
tanardi <tan.ardi94@gmail.com>
2019-04-26 22:18:43 +0700

    using blade

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,18 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create Post</title>
-</head>
-<body>
-    <h1>This is page to create post</h1>
+@extends('template')
+
+@section('content')
+<div class="content">
+<h1>This is page to create post</h1><br>
 <form action="{{route('post.store')}}" method="post">
         {{ csrf_field() }}
         <label for="title">Title</label><input type="text" name="title" id="title"><br>
         <label for="body">Body</label><input type="text" name="body" id="body" cols="30" rows="10"><br>
         <button type="submit">Submit</button>
     </form>
-</body>
-</html>
+</div>
+@endsection
